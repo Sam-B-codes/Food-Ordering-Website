fad345ccd16021d08a20260c02ab1861caff514b
Sam-B-codes <bhardwaajsaumya05@gmail.com>
2025-12-27 18:34:09 +0530

    frontend

---
+++ src/context/StoreContextProvider.jsx
@@ -0,0 +1,17 @@
+// import { StoreContext } from "./StoreContext";
+// import { food_list } from "../assets/assets";
+
+// const StoreContextProvider = ({ children }) => {
+
+//     const contextValue = {
+//         food_list
+//     };
+
+//     return (
+//         <StoreContext.Provider value={contextValue}>
+//             {children}
+//         </StoreContext.Provider>
+//     );
+// };
+
+// export default StoreContextProvider;
